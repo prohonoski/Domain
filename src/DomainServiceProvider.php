07382360e498c3f1c0984bfedf081ff132766327
Anderson Prohonoski <prohonoski@gmail.com>
2025-01-16 20:04:29 -0300

    primeiro commit

--- src/DomainServiceProvider.php
+++ src/DomainServiceProvider.php
@@ -1,20 +1,9 @@
+<?php
+
+namespace Proho\Domain;
+
+use Illuminate\Support\ServiceProvider;
+
+class DomainServiceProvider extends ServiceProvider
 {
-    "name": "seuvendor/seupacote",
-    "description": "Descrição do seu pacote",
-    "autoload": {
-        "psr-4": {
-            "SeuVendor\\SeuPacote\\": "src/"
-        }
-    },
-    "require": {
-        "php": "^7.3|^8.0"
-    },
-    "extra": {
-        "laravel": {
-            "providers": [
-                "SeuVendor\\SeuPacote\\SeuPacoteServiceProvider"
-            ],
-            "aliases": []
-        }
-    }
 }
